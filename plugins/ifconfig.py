5d657e5612cb1a8d1fdb4ddd77a3d76fe15daf3f
Amir <amir9339@gmail.com>
2021-10-10 11:56:35 +0300

    I finished working on ifconfig plugin!

--- plugins/ifconfig.py
+++ plugins/ifconfig.py
@@ -1,11 +1,16 @@
-from logging import exception
 from typing import List
+import logging
 
 from volatility3.framework import exceptions, renderers, constants, interfaces 
 from volatility3.framework.configuration import requirements
+from volatility3.framework.renderers import conversion
 from volatility3.framework.objects import utility
 
-MAX_STRING = 256
+MAX_STRING  = 256
+IFNAMSIZ    = 16 # This is a constant for if (interface) name size (a property of net_device struct)
+                 #  https://elixir.bootlin.com/linux/latest/source/include/linux/netdevice.h#L1932
+
+vollog = logging.getLogger(__name__)
 
 class Ifconfig(interfaces.plugins.PluginInterface):
     """ Ifconfig emulation plugin """
@@ -20,21 +25,43 @@ class Ifconfig(interfaces.plugins.PluginInterface):
         context: interfaces.context.ContextInterface,
         vmlinux_module_name: str):
         """ 
-        This function searches for net neamespaces list and extract all net devices from it
+        This function searches for net neamespaces lists and extract all net devices from them
         
         e.g, ns = Namespace
         """
 
         vmlinux = context.modules[vmlinux_module_name]
         net_namespace_list = vmlinux.object_from_symbol(symbol_name = "net_namespace_list") # Get net_namespace_list object which is a list_head object
+        
+        # Enumerate each network namespace (struct net) in memory and pass the first one 
         for i, net_ns in enumerate(net_namespace_list.to_list('symbol_table_name1!net', 'list', sentinel=False)):
             if i == 0:
                 continue
-            print(f'ifindex: {net_ns.ifindex}')
-            for interface in net_ns.dev_base_head.to_list('symbol_table_name1!net_device', 'dev_list', sentinel=True):
-                name = utility.array_to_string(interface.name, 16)
-                print(f'name: {name}')
-                yield interface
+
+            # Each network ns holds interfaces list. Walk each interface 
+            for interface in net_ns.dev_base_head.to_list('symbol_table_name1' + constants.BANG + 'net_device', 'dev_list', sentinel=True):
+                
+                # If appears
+                if interface.name:
+                    yield interface
+    
+    @classmethod
+    def _get_devs_base(self,         
+        context: interfaces.context.ContextInterface,
+        vmlinux_module_name: str):
+        """
+        ! This function has not been properly tested !
+        The function searches for net_device object and walk it's internal list
+        """
+
+        vmlinux = context.modules[vmlinux_module_name]
+        net_device_ptr = vmlinux.object_from_symbol(symbol_name = "dev_base")
+        net_device = net_device_ptr.dereference()
+
+        for net_dev in net_device.dev_list:
+            yield net_dev
+
+
 
     def _parse_mac_addr_from_net_dev(self, net_dev):
         """ 
@@ -62,44 +89,37 @@ class Ifconfig(interfaces.plugins.PluginInterface):
             https://stackoverflow.com/questions/59382141/obtain-interface-netmask-in-linux-kernel-module
         """
 
-        in_device = net_dev.ip_ptr.cast('in_device')
+        in_device = net_dev.ip_ptr
         ifa_list = in_device.ifa_list
 
         # TODO: FIX
         # Walk each interface in this list
         while ifa_list:
             ip_addr = ifa_list.ifa_address
-            name = ifa_list.ifa_label
+            ip_addr = conversion.convert_ipv4(ip_addr) # Convert IP address from intiger to a valid string
+            
+            name = ifa_list.ifa_label # Interface name from interface struct (struct in_ifaddr)
             name = utility.array_to_string(name)
-            print(ip_addr, name)
-
             ifa_list = ifa_list.ifa_next
+            return (name, ip_addr)
+        return "", ""
+
 
     def _gather_net_dev_info(self, net_dev):
-        mac_addr, promisc = None, None
+        """ 
+        This function gets a net_dev object and tries to extract the data from it using other functions.
+        If its get an "Invalid Page" exception it does nothing :()
+        """
 
         try:
             mac_addr = self._parse_mac_addr_from_net_dev(net_dev)
             promisc = str(self._parse_promisc_mode_from_net_dev(net_dev))            
-            #name, ip_addr = self._parse_ifa_value(net_dev)
+            name, ip_addr = self._parse_ifa_value(net_dev)
         except exceptions.PagedInvalidAddressException:
             pass
-        
-        try:
-            in_dev = net_dev.ip_ptr.cast('in_device') # Get inet (internet) device struct
-
-            ifa_list = in_dev.ifa_list
-            while ifa_list != None:
-                ifa_address = ifa_list.ifa_address
-                print(utility.array_to_string(ifa_address, 100))
-
-                ifa_list = ifa_list.ifa_next
-
-        except exceptions.PagedInvalidAddressException:
-                pass
-        
+                
         if mac_addr:
-            return "name", "ip_addr", mac_addr, promisc
+            return name, ip_addr, mac_addr, promisc
         return "0", "0", "0", "0"
                                
             
@@ -112,13 +132,24 @@ class Ifconfig(interfaces.plugins.PluginInterface):
         ]
 
     def _generator(self):
+
+        vmlinux_module_name = self.config['kernel']
+        vmlinux = self.context.modules[vmlinux_module_name]
+        
+        # Newer kernels
+        if vmlinux.has_symbol("net_namespace_list"):
+            func = self._get_devices_namespaces
+        elif vmlinux.has_symbol("dev_base"):
+            func = self._get_devs_base
+        else:
+            vollog.error("Unable to determine ifconfig information. Probably because it's an old kernel")
+            return
         
         # Gather all devices
-        for net_dev in self._get_devices_namespaces(self.context, self.config['kernel']):
+        for net_dev in func(self.context, self.config['kernel']):
             name, ip_addr, mac_addr, promisc = self._gather_net_dev_info(net_dev)
 
-            if name != "0":
-                yield (0, (name, ip_addr, mac_addr, promisc))
+            yield (0, (name, ip_addr, mac_addr, promisc))
 
     def run(self):
-        return renderers.TreeGrid([("Interface", str), ("IP Address", str), ("MAC Adress", str), ("Promiscous Mode", str)], self._generator())
+        return renderers.TreeGrid([("Interface name", str), ("IP Address", str), ("MAC Adress", str), ("Promiscous Mode", str)], self._generator())
