e4e4747ab9849329a77e82c3a16cd1db4e5f8b0d
Ofek Shaked <oshaked1@gmail.com>
2021-10-31 01:15:19 +0300

    Fixed a major issue with path calculations in linux.mount.Mount. Calculating a path now requires a task struct (this is how its done in the Linux kernel). Started adapting linux.file.ListFiles to the new path calculation method. Right now it can't be used without a PID filter.

--- plugins/pslist.py
+++ plugins/pslist.py
@@ -1,7 +1,7 @@
 # This file is Copyright 2019 Volatility Foundation and licensed under the Volatility Software License 1.0
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 #
-from typing import Callable, Iterable, List, Any, Tuple
+from typing import Callable, Iterable, List, Any
 from dataclasses import dataclass
 import logging
 from datetime import datetime
